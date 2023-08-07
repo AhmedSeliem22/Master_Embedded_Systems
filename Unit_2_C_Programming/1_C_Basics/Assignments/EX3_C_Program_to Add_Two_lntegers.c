ec3dcb6f36b1c67eb013f3a4bc7c37943f01444b
Hamad Sayed Issa <139787716+hamad-s-issa@users.noreply.github.com>
2023-08-07 20:43:52 +0300

    Update EX3_C_Program_to Add_Two_lntegers.c

--- Unit_2_C_Programming/1_C_Basics/Assignments/EX3_C_Program_to Add_Two_lntegers.c
+++ Unit_2_C_Programming/1_C_Basics/Assignments/EX3_C_Program_to Add_Two_lntegers.c
@@ -13,10 +13,10 @@ int main()
    int num1, num2, sum;
 
    printf("Enter two integers: ");
+   fflush(stdin);
    scanf("%d %d",&num1,&num2);
 
    sum = num1 + num2;
-
    printf("Sum: %d",sum);
 
    return 0;
